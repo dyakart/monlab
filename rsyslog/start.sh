cb1eeb16b189cc3bf491703d2bd5daa7e10508a1
filippovstar17 <filippovstar17@gmail.com>
2025-10-14 00:10:26 +0300

    v_0_4_2_0 | zabbix: all plugins check

--- rsyslog/start.sh
+++ rsyslog/start.sh
@@ -2,5 +2,10 @@
 set -e
 mkdir -p /run/zabbix /var/spool/rsyslog /var/log/remote
 chown -R zabbix:zabbix /run/zabbix || true
+
+# Устанавливаем права для группы logreaders
+chown -R root:logreaders /var/log/remote
+chmod -R 750 /var/log/remote  # Владелец: rwx, группа: r-x, другие: --
+
 (/usr/sbin/zabbix_agent2 -f -c /etc/zabbix/zabbix_agent2.conf &) >/dev/null 2>&1 || true
 exec rsyslogd -n -f /etc/rsyslog.conf
